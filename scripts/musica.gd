50049f0ab694cd8a6ba6632af9ea54e38b7dcdcd
Cleo Sánchez <83270683+Mantisette@users.noreply.github.com>
2023-05-28 14:03:25 +0200

    Transición de música
    Ahora la música cambia gradualmente.

--- scripts/musica.gd
+++ scripts/musica.gd
@@ -1,22 +1,38 @@
 extends Node2D
 
-const MIN_VOLUME = -80
-const MAX_VOLUME = 0
+const MIN_VOLUME = -40
+const MAX_VOLUME = -10
+const VOLUME_STEP = 2
 
 onready var musica_calma := $MusicaCalma
 onready var musica_combate := $MusicaCombate
 
+export var combate := false
 
 func _ready():
-  musica_calma.volume_db = MAX_VOLUME
   musica_combate.volume_db = MIN_VOLUME
+
+
+func _process(delta):
+  if !combate:
+    cambiar_volumen(musica_calma, MAX_VOLUME, true)
+    cambiar_volumen(musica_combate, MIN_VOLUME, false)
+  else:
+    cambiar_volumen(musica_calma, MIN_VOLUME, false)
+    cambiar_volumen(musica_combate, MAX_VOLUME, true)
+
+
+func cambiar_volumen(pista, objetivo, aumentar = false):
+  var volumen = pista.volume_db
+  if aumentar and volumen < objetivo:
+    pista.volume_db += VOLUME_STEP
+  elif !aumentar and volumen > objetivo:
+    pista.volume_db -= VOLUME_STEP
 
 
 func cambiar_musica_combate():
-  musica_calma.volume_db = MIN_VOLUME
-  musica_combate.volume_db = MAX_VOLUME
+  combate = true
 
 
 func cambiar_musica_calma():
-  musica_calma.volume_db = MAX_VOLUME
-  musica_combate.volume_db = MIN_VOLUME
+  combate = false
